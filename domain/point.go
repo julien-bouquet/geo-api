25a6d5ae50d9d4c59286710a8876e74523f3a756
Thyrael <robin.baraffe@gmail.com>
2021-10-26 17:05:22 +0200

    geo-api: add new attribute on point model

--- domain/point.go
+++ domain/point.go
@@ -3,7 +3,9 @@ package domain
 type Points []*Point
 
 type Point struct {
-	Name string  `json:"name" bson:"name"`
-	Lat  float32 `json:"lattitude" bson:"lattitude,truncate"`
-	Long float32 `json:"longitude" bson:"longitude,truncate"`
+	Name        string  `json:"name" bson:"name"`
+	Lat         float32 `json:"lattitude" bson:"lattitude,truncate"`
+	Long        float32 `json:"longitude" bson:"longitude,truncate"`
+	Type        string  `json:"type" bson:"type"`
+	Description string  `json:"description" bson:"description"`
 }
